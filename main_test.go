46525dea5483d28dc7625afd36b83af4ce5b9cc8
James Lawrence <jamesl@nutmeg.com>
2016-12-07 16:03:00 +0000

    Added more tests for the new functions.

--- main_test.go
+++ main_test.go
@@ -12,3 +12,31 @@ func TestMakeStringJSONSafe(t *testing.T) {
 	s = makeStringJSONSafe(s)
 	assert.Equal(t, s, sSafe)
 }
+
+func TestWriteMap(t *testing.T) {
+	m := map[string]string{"key1": "value1", "key2": "value2"}
+	s := `"element": {"key1":"value1","key2":"value2"},`
+	s2 := WriteMap("element", m)
+	assert.Equal(t, s, s2)
+}
+
+func TestWriteMapItems(t *testing.T) {
+	m := map[string]string{"key1": "value1", "key2": "value2"}
+	s := `"key1":"value1","key2":"value2"`
+	s2 := WriteMapItems(m)
+	assert.Equal(t, s, s2)
+}
+
+func TestWriteSlice(t *testing.T) {
+	sl := []string{"value1", "value2"}
+	s := `"element": ["value1","value2"],`
+	s2 := WriteSlice("element", sl)
+	assert.Equal(t, s, s2)
+}
+
+func TestWriteSliceItems(t *testing.T) {
+	sl := []string{"value1", "value2"}
+	s := `"value1","value2"`
+	s2 := WriteSliceItems(sl)
+	assert.Equal(t, s, s2)
+}
