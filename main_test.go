9c4356b9d9e0dcf6a13d714ef2348c73caef68d8
James Lawrence <jamesl@nutmeg.com>
2016-12-14 08:16:53 +0000

    Added test for the replacement variables.

--- main_test.go
+++ main_test.go
@@ -40,3 +40,11 @@ func TestWriteSliceItems(t *testing.T) {
 	s2 := WriteSliceItems(sl)
 	assert.Equal(t, s, s2)
 }
+
+func TestReplacePlaceholders(t *testing.T) {
+	testString := "We are the {{what_are_we}}, And we are the {{what_are_we_also}}"
+	expectedOutput := "We are the music makers, And we are the dreamers of dreams"
+	testMap := map[string]string{"what_are_we": "music makers", "what_are_we_also": "dreamers of dreams"}
+	output := replacePlaceholders([]byte(testString), testMap)
+	assert.Equal(t, string(output), expectedOutput)
+}
